0191f10d054eea5e9ab38f5f79eb46612c64a3a9
unknown <hussienmo385gmial.com>
2020-04-27 12:23:09 +0200

    mobile header done

--- js/mobile.js
+++ js/mobile.js
@@ -1,40 +1,36 @@
 $(document).ready(function() {
+    // from here
     $(".search-icon").on("click", function() {
-            $(".search-field").slideToggle("", function() {
-                $(".search-history").toggle()
-            });
-
-        })
-        // $(".search-input input").on("change", function() {
-        //     var search = $(this).val()
-        //     $(".history").text(search)
-        // })
+        $(".search-field").slideToggle("", function() {
+            $(".search-history").toggle();
+        });
+    });
 
     $(".search-input input").on("focus", function() {
-        $(".search-history").hide()
-    })
+        $(".search-history").hide();
+    });
     $(".search-input input").on("blur", function() {
-        $(".search-history").show()
-    })
+        $(".search-history").show();
+    });
 
     $(".remove-history").on("click", function() {
-        $(".second-history").hide()
-        $(".history").text("")
-    })
+        $(".second-history").hide();
+        $(".history").text("");
+    });
     $(".bars").on("click", function() {
-        $(".links").css("right", "0")
-    })
+        $(".links").css("right", "0");
+    });
     $(".links .close").on("click", function() {
-        $(".links").css("right", "-100%")
-    })
+        $(".links").css("right", "-100%");
+    });
     $(".main-links .main-nav-link:not(:last-child)").on("click", function() {
-        $(".main-links").hide()
-        $(".sup-links").show()
-    })
+        $(".main-links").hide();
+        $(".sup-links").show();
+    });
     $(".country").on("click", function() {
-        $(".choose-country").show()
-    })
+        $(".choose-country").show();
+    });
     $(".choose-country .close-country").on("click", function() {
-        $(".choose-country").hide()
-    })
-})
+        $(".choose-country").hide();
+    });
+});
